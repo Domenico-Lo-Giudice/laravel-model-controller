57a2a06457c49f1621271a18ebb7757411a8cee7
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-03-29 14:06:25 +0200

    Add movies from database & view

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
-    //
+    public function movielist() {
+       
+        // SELECT * FROM `movies`
+        $movies = Movie::all();
+
+        dd($movies);
+
+        return view(`movielist`, compact(`movies`));
+        
+    }
 }
